1aaecf9aa9c30f34c64697d7d40c343433ba690a
sukiboo <dereventsov@gmail.com>
2023-01-13 16:41:12 -0500

    initial file upload

---
+++ main_tf.py
@@ -0,0 +1,55 @@
+
+import numpy as np
+import tensorflow as tf
+
+from smoothing_based_optimization import SmoothingBasedOptimization
+
+
+def tf_to_opt(model, data):
+    '''reframe model training as an optimization problem'''
+    # translate weights to array
+    weights_to_array = lambda weights:\
+        np.concatenate([weight.flatten() for weight in weights])
+
+    # define initial guess
+    weights = model.get_weights()
+    x0 = weights_to_array(weights)
+
+    # translate array to weights
+    shapes = [weight.shape for weight in weights]
+    sizes = [weight.size for weight in weights]
+    array_to_weights = lambda array:\
+        [a.reshape(shapes[i]) for i,a in enumerate(np.split(array, np.cumsum(sizes)[:-1]))]
+
+    # define target function
+    def fun(array):
+        model.set_weights(array_to_weights(array))
+        mse = model.evaluate(*data, verbose=0)
+        return mse
+
+    return fun, x0
+
+
+if __name__ == '__main__':
+
+    # sample training data
+    x = np.linspace(-np.pi, np.pi, 1001)
+    y = np.sin(x) * np.exp(x)
+
+    # setup model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input((None,1)),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1, activation=None)])
+    model.compile(loss='mse')
+
+    # get target function and initial guess
+    fun, x0 = tf_to_opt(model, (x,y))
+
+    # setup smoothing-based optimization
+    params = {'sigma': .1, 'learning_rate': .01, 'quad_points': 5, 'num_iters': 100}
+    sbo = SmoothingBasedOptimization(params)
+    sbo.minimize(fun, x0, plot=True)
+
---
+++ smoothing_based_optimization.py
@@ -0,0 +1,58 @@
+
+import numpy as np
+import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(style='darkgrid', palette='muted', font='monospace')
+
+
+class SmoothingBasedOptimization:
+
+    def __init__(self, params):
+        self.__dict__.update(params)
+
+    def minimize(self, fun, x0, plot=False):
+        '''minimize the given function fun with the initial guess x0'''
+        self.fun = fun
+        self.x = x0
+        self.dim = x0.size
+
+        # establish search directions and quadrature points
+        basis = np.eye(self.dim)
+        gh_roots, gh_weights = np.polynomial.hermite.hermgauss(self.quad_points)
+
+        # iteratively minimize the function
+        self.vals = [self.fun(self.x)]
+        pbar = tqdm.trange(self.num_iters, ascii=True, postfix=f'value={self.vals[-1]: .2e}')
+        for _ in pbar:
+
+            # estimate smoothed directional derivative along each basis direction
+            df_sigma_basis = np.zeros(self.dim)
+            for d in range(self.dim):
+
+                # estimate directional derivative via Gauss--Hermite quadrature
+                f_d = lambda t: self.fun(self.x + t*basis[d])
+                f_d_vals = np.array([f_d(self.sigma * p) for p in gh_roots])
+                df_sigma_basis[d] = np.sum(gh_weights * gh_roots * f_d_vals)\
+                                    / (self.sigma * np.sqrt(np.pi)/2)
+
+            # assemble smoothed gradient and update minimizer
+            df_sigma = np.matmul(basis, df_sigma_basis)
+            self.x -= self.learning_rate * df_sigma
+
+            # report progress
+            self.vals.append(self.fun(self.x))
+            pbar.set_postfix(value=f'{self.vals[-1]: .2e}')
+
+        if plot:
+            self.plot_values()
+
+    def plot_values(self):
+        '''plot function values throughout the optimization'''
+        fig, ax = plt.subplots(figsize=(8,5))
+        ax.plot(self.vals, linewidth=3)
+        ax.set_yscale('log')
+        plt.tight_layout()
+        plt.show()
+
