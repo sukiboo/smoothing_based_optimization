32f2327aed619b8ff63f88bf313e24c29bf6d750
sukiboo <dereventsov@gmail.com>
2023-02-27 17:11:31 -0500

    incorporate target and noise functions into benchmarking

--- benchmark_algorithms.py
+++ benchmark_algorithms.py
@@ -13,21 +13,8 @@ sns.set_theme(style='darkgrid', palette='muted', font='monospace')
 
 class BenchmarkAlgorithms:
 
-    def __init__(self, target, noise, domain):
-        self.fun = lambda x: target(x) + noise(x)
-        self.domain = np.array(domain)
-
-    def test(self, num_tests, random_seed):
-        '''solve an optimization problem with various initial guesses'''
-        self.setup_optimization_params(num_tests, random_seed)
-        self.vals = {alg: [] for alg in self.algos}
-        for alg in self.algos:
-            for t in tqdm.trange(num_tests, desc=f'Testing {alg}', ascii=True):
-                self.sample_initial_guess(t)
-                self.run_minimization(alg)
-
-    def setup_optimization_params(self, num_tests, random_seed):
-        '''configure benchmarking parameters'''
+    def __init__(self, dim, num_tests, random_seed):
+        self.dim = dim
         np.random.seed(random_seed)
         self.random_seed_list = np.random.randint(1e+9, size=num_tests)
         num_iters = 100
@@ -35,12 +22,31 @@ class BenchmarkAlgorithms:
         self.sbo_params = {'sigma': .1, 'learning_rate': .01,
                            'quad_points': 7, 'num_iters': num_iters}
         self.scipy_params = {'maxiter': num_iters}
+        self.test_algorithms()
 
-    def sample_initial_guess(self, t):
+    def test_algorithms(self):
+        '''generate and solve an optimization problem'''
+        self.vals = {alg: [] for alg in self.algos}
+        for alg in self.algos:
+            for t in tqdm.trange(num_tests, desc=f'Testing {alg}', ascii=True):
+                np.random.seed(self.random_seed_list[t])
+                self.generate_function()
+                self.sample_initial_guess()
+                self.run_minimization(alg)
+
+    def generate_function(self):
+        '''generate optimization target by perturbing sphere function'''
+        dilation_sphere = 10 * np.random.rand(self.dim)
+        dilation_noise = 10 * np.random.randn(self.dim)
+        target = lambda x: np.sum(dilation_sphere * x**2)
+        noise = lambda x: np.sin(np.sum(np.abs(dilation_noise * x)))
+        ##noise = lambda x: np.random.randn()
+        self.fun = lambda x: target(x) + noise(x)
+
+    def sample_initial_guess(self):
         '''randomly sample an initial guess from the domain'''
-        np.random.seed(self.random_seed_list[t])
-        self.x0 = np.random.rand(len(self.domain))\
-            * (self.domain[:,1] - self.domain[:,0]) + self.domain[:,0]
+        domain = np.array([[-1,1]] * self.dim)
+        self.x0 = np.random.rand(self.dim) * (domain[:,1] - domain[:,0]) + domain[:,0]
 
     def run_minimization(self, alg):
         '''perform minimization with a given algorithm'''
@@ -79,45 +85,7 @@ class BenchmarkAlgorithms:
 
 if __name__ == '__main__':
 
-    # configure benchmark parameters
-    target = lambda x: np.sum(x**2)
-    ##noise = lambda x: 0
-    ##noise = lambda x: np.prod(1 + np.sin(x))
-    noise = lambda x: np.sin(np.sum(10*np.abs(x)))
-    ##noise = lambda x: np.random.randn()
-
-    domain = [[-1,1]] * 100
-    num_tests = 10
-    random_seed = 0
-
-
-
-
-    '''numerical examples from https://arxiv.org/abs/2302.06404'''
-    ### first test
-    ##a = 1
-    ##target = lambda x: np.sqrt(np.sum(np.abs(x[i])**(3+i) for i in range(5)))
-    ##noise = lambda x: np.sum(np.sin(2*np.pi*a*x[i]) for i in range(5))
-    ##domain = [[-20,20]] * 5
-
-    ### second test
-    ##a = 1
-    ##A = np.random.rand(5,20) / a
-    ##target = lambda x: np.sqrt(np.sum(np.abs(x[i])**(3+i) for i in range(5)))
-    ##noise = lambda x: 1/20 * np.sum(np.sum(np.sin(2*np.pi*A[i][j]*x[i])\
-                                           ##for i in range(5)) for j in range(20))
-    ##domain = [[-20,20]] * 5
-
-    ### third test
-    ##target = lambda x: np.sum(x**2)
-    ##noise = lambda x: np.sum(x**2 * np.sin(2*np.pi*x))
-    ##domain = [[-5,5]] * 5
-
-
-
-
     # run benchmarking
-    ba = BenchmarkAlgorithms(target, noise, domain)
-    ba.test(num_tests, random_seed)
+    ba = BenchmarkAlgorithms(dim, num_tests, random_seed)
     ba.visualize()
 
